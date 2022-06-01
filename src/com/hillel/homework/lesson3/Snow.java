0deb7a97650ae1b204c974ce2c06ef0d9f46ebd7
Oleksandr Mansarliiskyi <a.mansarliyskiy@gmail.com>
2022-06-01 17:10:48 +0300

    add 3 version calc

--- src/com/hillel/homework/lesson3/Snow.java
+++ src/com/hillel/homework/lesson3/Snow.java
@@ -18,7 +18,25 @@ public class Snow {
         float m2 = p * (v1 + v2 + v3);
         System.out.println("m2=" + m2 + "kg");
 
+        float M1 = calcMass(0.2f, 0.7f);
+        float M2 = calcMass(0.5f, 0.7f);
+        float M3 = calcMass(1, 0.7f);
+        System.out.println("m3=" + calcVolume(M1, M2, M3) + "kg");
     }
+
+    //3 вариант
+    public static float calcMass(float r, float p) {
+        float M = (float) (p * (float) 4 / 3 * Math.PI * Math.pow(r, 3));
+        return M;
+    }
+
+    public static float calcVolume(float M1, float M2, float M3) {
+        float M = (M1 + M2 + M3);
+        return M;
+    }
+
+
 }
+
 
 
