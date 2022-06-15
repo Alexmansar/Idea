eb3577197201262dfc63cfd85c75d265aedfb522
Oleksandr Mansarliiskyi <a.mansarliyskiy@gmail.com>
2022-06-15 17:43:43 +0300

    Add CW9

---
+++ src/com/hillel/classwork/lesson9/ArraySort.java
@@ -0,0 +1,46 @@
+package com.hillel.classwork.lesson9;
+
+import java.util.Arrays;
+
+public class ArraySort {
+
+    public static void main(String[] args) {
+        int[] number = new int[100000];
+
+        int temp = 0;
+        fill(number);
+
+        long start = System.currentTimeMillis();
+     //   bubbleSort(number);
+//        Arrays.sort(number);
+
+        long finish = System.currentTimeMillis();
+        System.out.println("time: " + (finish - start));
+
+        // System.out.println(Arrays.toString(number));
+    }
+
+    public static void fill(int[] number) {
+        for (int i = 0; i < number.length; i++) {
+            number[i] = (int) (Math.random() * 100);
+        }
+    }
+
+    public static void bubbleSort(int[] number) {
+        int temp;
+        for (int i = 0; i < number.length; i++) {
+
+            for (int j = 1; j < (number.length - i); j++) {
+
+                if (number[j - 1] > number[j]) {
+                    temp = number[j - 1];
+                    number[j - 1] = number[j];
+                    number[j] = temp;
+                }
+
+            }
+
+        }
+    }
+
+}
